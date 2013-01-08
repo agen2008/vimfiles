8b94c38f8499aecc000e39f51d43034b21d2c7f9
root <root@debian00.(none)>
2013-01-08 15:46:37 +0800

    del install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-ln -s ~/vimfiles ~/.vim
-echo "source ~/.vim/vimrc" > ~/.vimrc
-echo "source ~/.vim/gvimrc" > ~/.gvimrc
-cd ~/.vim
-git submodule init
-git submodule update
-
-vim +BundleInstall +qall
