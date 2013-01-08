5dc1d01077cc6cb0f6aed8977306aa69e0b6b4e4
root <root@debian00.(none)>
2013-01-08 15:49:21 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# backup existed config files
+for i in .vim vimfiles .vimrc .gvimrc; do
+  # file or symbol link
+  if [ -e ~/$i -o -h ~/$i ]; then
+    # rm old backup
+    if [ -e ~/$i.old ]; then
+      rm -rf ~/$i.old
+    fi
+    # backup file or folder
+    mv -f ~/$i ~/$i.old
+  fi
+done
+
+git clone git://github.com/wongyouth/vimfiles ~/vimfiles
+ln -s ~/vimfiles ~/.vim
+echo "source ~/.vim/vimrc" > ~/.vimrc
+echo "source ~/.vim/gvimrc" > ~/.gvimrc
+cd ~/.vim
+git submodule init
+git submodule update
+
+vim +BundleInstall +qall
